443f380cdd2f92eb757bae92ef51736fa3064b9b
J. Santos <jamillo@gmail.com>
2019-09-28 12:45:07 -0300

    :recycle: Update how schemaUser is initialized

--- tests/schema.go
+++ tests/schema.go
@@ -4,23 +4,37 @@ import "github.com/jamillosantos/go-ceous"
 
 type schemaUser struct {
 	*ceous.BaseSchema
-	ID   ceous.SchemaField
-	Name ceous.SchemaField
+	ID        ceous.SchemaField
+	Name      ceous.SchemaField
+	Password  ceous.SchemaField
+	Role      ceous.SchemaField
+	CreatedAt ceous.SchemaField
+	UpdatedAt ceous.SchemaField
 }
 
 type schema struct {
 	User *schemaUser
 }
 
+var userBaseSchema = ceous.NewBaseSchema(
+	"users",
+	"",
+	ceous.NewSchemaField("id"),
+	ceous.NewSchemaField("name"),
+	ceous.NewSchemaField("password"),
+	ceous.NewSchemaField("role"),
+	ceous.NewSchemaField("created_at"),
+	ceous.NewSchemaField("updated_at"),
+)
+
 var Schema = schema{
 	User: &schemaUser{
-		BaseSchema: ceous.NewBaseSchema(
-			"users",
-			"",
-			ceous.NewSchemaField("id"),
-			ceous.NewSchemaField("name"),
-		),
-		ID:   ceous.NewSchemaField("id"),
-		Name: ceous.NewSchemaField("name"),
+		BaseSchema: userBaseSchema,
+		ID:         userBaseSchema.ColumnsArr[0],
+		Name:       userBaseSchema.ColumnsArr[1],
+		Password:   userBaseSchema.ColumnsArr[2],
+		Role:       userBaseSchema.ColumnsArr[3],
+		CreatedAt:  userBaseSchema.ColumnsArr[4],
+		UpdatedAt:  userBaseSchema.ColumnsArr[5],
 	},
 }
