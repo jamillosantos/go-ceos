2676c112a07f5479f39b981cca3a3f6770df4f67
J. Santos <jamillo@gmail.com>
2020-02-11 15:39:44 -0300

    Removes name functions from helpers

--- generator/helpers/helpers.go
+++ generator/helpers/helpers.go
@@ -1,21 +1,5 @@
 package helpers
 
-import "strings"
-
-func CamelCase(name string) string {
-	if name == "" {
-		return ""
-	}
-	return strings.ToLower(name[0:1]) + name[1:]
-}
-
-func PascalCase(name string) string {
-	if name == "" {
-		return ""
-	}
-	return strings.ToUpper(name[0:1]) + name[1:]
-}
-
 func AppendStringIfNotEmpty(arr []string, data ...string) []string {
 	for _, s := range data {
 		if s != "" {
